b0dbe800246d01bc454fde71113dc1741f353612
p4ck <muhammadpailus@gmail.com>
2021-08-19 20:38:06 +0700

    django heroku

--- blog/settings.py
+++ blog/settings.py
@@ -14,7 +14,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import django_heroku
-django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -145,3 +144,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+django_heroku.settings(locals())
