9b46247dc373aac06ff14362428586043aaff170
p4ck <muhammadpailus@gmail.com>
2021-08-20 19:31:27 +0700

    string

--- post/serializer.py
+++ post/serializer.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import *
 
 class PostSerializer(serializers.ModelSerializer):
+    user_nama = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Post
         fields = "__all__"
-
+    def get_user_nama(self, obj):
+        if obj:
+            return obj.username
+        return None
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
